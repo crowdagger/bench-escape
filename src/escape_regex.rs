03ed98763fe8bf6814629b39422e74ccaf2d9535
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-04 23:27:38 +0200

    Add regex without lazy_static

--- src/escape_regex.rs
+++ src/escape_regex.rs
@@ -25,3 +25,51 @@ pub fn find<'a, S: Into<Cow<'a, str>>>(input: S) -> Cow<'a, str> {
         input.into()
     }
 }
+
+pub fn find_no_static<'a, S: Into<Cow<'a, str>>>(input: S) -> Cow<'a, str> {
+    let regex: Regex = Regex::new("[<>&]").unwrap();
+    let input = input.into();
+    let first = regex.find(&input);
+    if let Some((first, _)) = first {
+        let mut output = String::from(&input[0..first]);
+        output.reserve(input.len() - first);
+        let rest = input[first..].chars();
+        for c in rest {
+            match c {
+                '<' => output.push_str("&lt;"),
+                '>' => output.push_str("&gt;"),
+                '&' => output.push_str("&amp;"),
+                _ => output.push(c),
+            }
+        }
+
+        Cow::Owned(output)
+    } else {
+        input.into()
+    }
+}
+
+
+pub fn find_no_reserve<'a, S: Into<Cow<'a, str>>>(input: S) -> Cow<'a, str> {
+    lazy_static! {
+        static ref REGEX: Regex = Regex::new("[<>&]").unwrap();
+    }
+    let input = input.into();
+    let first = REGEX.find(&input);
+    if let Some((first, _)) = first {
+        let mut output = String::from(&input[0..first]);
+        let rest = input[first..].chars();
+        for c in rest {
+            match c {
+                '<' => output.push_str("&lt;"),
+                '>' => output.push_str("&gt;"),
+                '&' => output.push_str("&amp;"),
+                _ => output.push(c),
+            }
+        }
+
+        Cow::Owned(output)
+    } else {
+        input.into()
+    }
+}
